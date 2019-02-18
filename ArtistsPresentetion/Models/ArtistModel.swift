b438d83c0ff471f6a4cfeae68a6b29d034eaed01
Андрей Романюк <andrejromanuk@macbook-pro-andrej.local>
2019-02-18 19:24:38 +0300

    Adding artist presentation - in process

---
+++ ArtistsPresentetion/Models/ArtistModel.swift
@@ -0,0 +1,37 @@
+//
+//  ArtistModel.swift
+//  ArtistsPresentetion
+//
+//  Created by Андрей Романюк on 2/18/19.
+//  Copyright © 2019 Андрей Романюк. All rights reserved.
+//
+
+import Foundation
+
+class Artist: Codable{
+    private var thumb_url = String()
+    private var mbid = String()
+    private var facebook_page_url = String()
+    private var image_url = String()
+    private var name = String()
+    private var id = String()
+    private var tracker_count = Int()
+    private var upcoming_event_count = Int()
+    private var url = String()
+    
+    func getName()->String{
+        return self.name
+    }
+    func getImageUrl()->String{
+        return self.image_url
+    }
+    func getFacebookPage()->String{
+        return self.facebook_page_url
+    }
+    func getUpcomingEventCount()->Int{
+        return self.upcoming_event_count
+    }
+    func getURL()->String{
+        return self.url
+    }
+}
--- ArtistsPresentetion/ViewControllers/SearchViewController.swift
+++ ArtistsPresentetion/ViewControllers/SearchViewController.swift
@@ -10,17 +10,63 @@ import UIKit
 
 class SearchViewController: UIViewController, UISearchBarDelegate {
 
+    // MARK: - Vars
+    var currentArtist:Artist?
+    var currentSearchText = String()
+    
     // MARK: - Outlets
     @IBOutlet weak var searchBar: UISearchBar!{
         didSet{
             searchBar.delegate = self
         }
     }
+    @IBOutlet weak var searchResultsLabel: UILabel!
+    @IBOutlet weak var searchSpinner: UIActivityIndicatorView!
+    @IBOutlet weak var presentationView: UIView!
     
     //MARK: - Search Bar & Keyboard
-    
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
+        searchSpinner.startAnimating()
+        searchResultsLabel.isHidden = true
+        searchAndPresentArtist()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.count == 0{
+            if searchSpinner.isAnimating{
+                searchSpinner.stopAnimating()
+            }
+            if searchResultsLabel.isHidden{
+                searchResultsLabel.isHidden = false
+            }
+            searchResultsLabel.text = "Search"
+        }else if (1..<3).contains(searchText.count){
+            if searchSpinner.isAnimating{
+                searchSpinner.stopAnimating()
+            }
+            if searchResultsLabel.isHidden{
+                searchResultsLabel.isHidden = false
+            }
+            searchResultsLabel.text = "Enter name"
+            if let url = URL(string: "https://google.com"){
+                let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+                    if error != nil{
+                        DispatchQueue.main.async {
+                            self.presentConnectionAlert()
+                        }
+                    }
+                }
+                task.resume()
+            }
+        }else{
+            searchSpinner.startAnimating()
+            if currentSearchText != searchText {
+                currentSearchText = searchText
+                searchResultsLabel.isHidden = true
+                searchAndPresentArtist()
+            }
+        }
     }
     
     // MARK: - View Controller
@@ -42,6 +88,47 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
         }
     }
     
+    // MARK: - MyTools
+    func presentConnectionAlert(){
+        let alert = UIAlertController(title: "Internet Connection", message: "There is a problem with internet connection. Please, turn ON cellular or connect to WiFi", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Settings", style: .default, handler: { (action) in
+            UIApplication.shared.open(URL(string: "App-Prefs:")!)
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func searchAndPresentArtist(){
+        if let codedSearchText = currentSearchText.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed){
+            if let url = URL(string: "https://rest.bandsintown.com/artists/\(codedSearchText)?app_id=ArtEve"){
+                let task = URLSession.shared.dataTask(with: url) {(artistData, response, error) in
+                    if error != nil{
+                        DispatchQueue.main.async {
+                            self.presentConnectionAlert()
+                            self.searchSpinner.stopAnimating()
+                        }
+                    }else{
+                        if let data = artistData{
+                            if let artist = try? JSONDecoder().decode(Artist.self, from: data){
+                                self.currentArtist = artist
+                                DispatchQueue.main.async {
+                                    self.searchSpinner.stopAnimating()
+                                }
+                            }else{
+                                DispatchQueue.main.async {
+                                    if self.searchSpinner.isAnimating{
+                                        self.searchSpinner.stopAnimating()
+                                    }
+                                    self.searchResultsLabel.text = "No results"
+                                }
+                            }
+                        }
+                    }
+                }
+                task.resume()
+            }
+        }
+    }
     
     /*
     // MARK: - Navigation
